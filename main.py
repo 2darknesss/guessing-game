980b3368a5f0f629432a566d6f6299ccfe6f7f1a
Piotr Brudny <piotr.brudny.ata@gmail.com>
2026-05-28 07:46:06 +0000

    add input

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import random
+
+
 def main():
-    print("Hello from guessing-game!")
+    print("The game is started")
+
+    num = random.randint(1,99)
+    users_number = input("Guess the number: ")
+    if num == users_number:
+        print("You won")
+    else: print("You lose")
+
+    
 
 
 if __name__ == "__main__":

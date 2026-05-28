2129046294e7017d767394fda49b58f6d0c5160e
Piotr Brudny <piotr.brudny.ata@gmail.com>
2026-05-28 08:50:33 +0000

    add hint

--- main.py
+++ main.py
@@ -5,12 +5,33 @@ def main():
     print("The game is started")
 
     num = random.randint(1,99)
-    users_number = input("Guess the number: ")
-    if num == users_number:
-        print("You won")
-    else: print("You lose")
+    while True:     
+        users_number = input("Guess the number: ")
+        if users_number == 'exit':
+            break
+        elif users_number== 'is the number even':
+            if num % 2 ==0 :
+                print('The number is even')
+            else:
+                print("the number is odd ")
+        elif num == users_number:
+            print("You won")
+        else: 
+            print("You lose")
 
+        
     
+
+    # TODO: Features 
+    # use loop
+    # take input for finishing the game
+    # take input for hint question 
+    # add scoring mechanism 
+
+    # TODO: Refactor
+    # extract classes
+    # use separate files
+
 
 
 if __name__ == "__main__":
